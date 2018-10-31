1b5bf8de6cc49d978b2d6689183b3035c11eedec
mk1808 <monikor97@gmail.com>
2018-10-31 08:48:25 +0100

    #8 dodanie wysiwtlania pytan,

--- View/src/app/content/test/question/question.component.ts
+++ View/src/app/content/test/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Answer } from 'src/app/shared/models/classes';
 
 @Component({
   selector: 'app-question',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
-
-  constructor() { }
+answers:Answer[]=[];
+colors:string[]=[];
+  constructor() {
+    for (let i=0; i<4; i++){
+      let answer= new Answer;
+      answer.id=i*3;
+      answer.text="Sweet in roast, dripper, aged, aromatic black robust brewed filter galão medium.";
+      this.answers.push(answer);
+    }
+   }
 
   ngOnInit() {
+    for (let i=0; i<this.answers.length; i++)
+    {
+      let color:string;
+      color="white";
+      this.colors.push(color);
+    }
   }
+public getStatus(id:number){
 
+  if(document.readyState)
+  {
+  if (this.colors[id]!='white'){
+    this.colors[id]= 'white';
+  }
+  else{
+    this.colors[id]= '#649494';
+  }
+}
+}
 }
