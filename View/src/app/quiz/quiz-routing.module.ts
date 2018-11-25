6db9e210eac79eb1291d6842b46f08f6bb726166
mk1808 <monikor97@gmail.com>
2018-11-25 16:35:47 +0100

    #21  widok logowania

--- View/src/app/quiz/quiz-routing.module.ts
+++ View/src/app/quiz/quiz-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-
-
+import { LoginComponent } from './auth/login/login.component'
+import { RegisterComponent } from './auth/register/register.component'
 const routes: Routes = [
   {
     path: "", component: LayoutComponent,children:[ {
@@ -12,6 +12,14 @@ const routes: Routes = [
       {
       path: "creating",
       loadChildren: "./creating/creating.module#CreatingModule"
+    },
+    {
+      path: "login",
+      component: LoginComponent 
+    },
+    {
+      path: "register",
+      component: RegisterComponent 
     },
     { path: '', redirectTo: '/quiz/begin', pathMatch: 'full'}, 
    ]
--- View/src/app/quiz/quiz.module.ts
+++ View/src/app/quiz/quiz.module.ts
@@ -4,17 +4,25 @@ import { LayoutComponent } from './layout/layout.component';
 import { QuizRoutingModule } from './quiz-routing.module';
 import { HeaderComponent } from './layout/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   imports: [
     CommonModule,
     QuizRoutingModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
+
+
   ],
   declarations: [LayoutComponent,HeaderComponent, FooterComponent, LoginComponent, RegisterComponent]
 })
