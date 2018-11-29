3bbcd3da0123c54b34667d635858e1474bdba087
mk1808 <monikor97@gmail.com>
2018-11-29 14:47:36 +0100

    #21 #23 podpiecie endpointow do logowania i rejestracji

--- View/src/app/quiz/auth/register/register.component.ts
+++ View/src/app/quiz/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../shared/models/classes';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    passwordRepeat: ['', Validators.required],
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+    email: ['', Validators.required]
 
-  constructor() { }
+  });
+  constructor(private fb: FormBuilder, private auth:AuthService) { }
 
   ngOnInit() {
+  }
+
+  onRegister(){
+    this.auth.register(this.registerForm.controls.username.value,
+      this.registerForm.controls.password.value,
+      this.registerForm.controls.email.value, 
+      this.registerForm.controls.name.value,
+      this.registerForm.controls.surname.value
+      ).subscribe
+    (x=>console.log(x), e=>console.log(e));
   }
 
 }
---
+++ View/src/app/quiz/shared/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/index';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+  public logIn(email:string, password:string): Observable<any> {
+    return this.http.post<any>('http://localhost/web/web/api/controllers/auth/login.php', 
+    {email:email, password:password},{headers:{"Access-Control-Allow-Origin":"*"}});
+}
+public register(email:string, password:string, 
+  name:string, surname:string, username:string ): Observable<any> {
+  return this.http.post<any>('http://localhost/web/web/api/controllers/auth/createUser.php', 
+  {email:email, password:password, name:name, surname:surname},{headers:{"Access-Control-Allow-Origin":"*"}});
+}
+}
