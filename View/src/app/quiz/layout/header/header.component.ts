84dbd142357b4f7cb90382eb2e59a101e1bc44a4
marqos12 <marqos111222@gmail.com>
2018-12-17 22:44:37 +0100

    #5 poprawa górnej belki

--- View/src/app/quiz/layout/header/header.component.ts
+++ View/src/app/quiz/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+import {ActivatedRoute, NavigationEnd, NavigationStart, Router, RoutesRecognized} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  logged:boolean = false;
+  name:string ;
+  constructor(private cookie:CookieService,private router:Router, private route:ActivatedRoute) {
+    this.router.events.subscribe(event => {
+        //calls this stuff when navigation ends
+      if (event instanceof RoutesRecognized)
+        this.checkLogged();
+    });
+  }
 
   ngOnInit() {
+    this.checkLogged();
+  }
+
+  checkLogged(){
+    if(this.cookie.get('user')=="") this.logged = false;
+    else {
+      this.logged = true;
+      this.name = JSON.parse(this.cookie.get('user')).name+" "+JSON.parse(this.cookie.get('user')).surname;
+    }
+  }
+
+  wyloguj(){
+    this.cookie.set("jwt", '',null,'/');
+    this.cookie.set("user",'',null,'/');
+    this.router.navigate(['/login']);
+  }
+
+  menuAction(button){
+    console.log(button);
   }
 
 }
