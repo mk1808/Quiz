a8ce8d5302be178c333b8cad9dafae4aaf9af660
marqos12 <marqos111222@gmail.com>
2018-12-25 21:06:58 +0100

    #54 dodanie weryfikacji jwt

--- web/web/api/controllers/question/createQuestion.php
+++ web/web/api/controllers/question/createQuestion.php
@@ -7,7 +7,6 @@
  */
 
 include_once '../../config/postConfig.php';
-
 include_once '../../models/question.php';
 
 $question = new Question($db);
@@ -21,13 +20,19 @@ $question->code = $data->code;
 $question->image = $data->image;
 $question->answers = $data->answers;
 
-$stmt = $question->create();
+$auth2 = authorizate($data->jwt);
+if (!$auth ||(isset($auth2["decoded"]) && ($auth2["decoded"]->role == 1)) ) {
 
-if($stmt>0){
-    http_response_code(200);
-    echo json_encode(array("message"=>"Question was created", "id"=>$stmt));
-}
-else{
-    http_response_code(400);
+    $stmt = $question->create();
+
+    if ($stmt > 0) {
+        http_response_code(200);
+        echo json_encode(array("message" => "Question was created", "id" => $stmt));
+    } else {
+        http_response_code(201);
+        echo json_encode(array("message" => "Unable to create question."));
+    }
+} else {
+    http_response_code(201);
     echo json_encode(array("message" => "Unable to create question."));
 }
--- web/web/api/controllers/question/getQuestionWithAnswers.php
+++ web/web/api/controllers/question/getQuestionWithAnswers.php
@@ -17,50 +17,61 @@ include_once '../../models/category.php';
 $data = json_decode(file_get_contents("php://input"));
 $questionObj= new Question($db);
 
-$category = new Category($db);
-$stmtQ = $questionObj->getQuestion($data->id);
-$num = $stmtQ->rowCount();
+$auth2 = authorizate($data->jwt);
+if (!$auth ||(isset($auth2["decoded"]))) {
 
-if($num>0){
-    while ($row = $stmtQ->fetch(PDO::FETCH_ASSOC)){
 
-        $stmtA = $questionObj->getAnswers($row['ID']);
+    $category = new Category($db);
+    $stmtQ = $questionObj->getQuestion($data->id);
+    $num = $stmtQ->rowCount();
 
-        $answers = array();
-        while($rowA = $stmtA->fetch(PDO::FETCH_ASSOC)){
-            $answer = array(
-                "id"=>$rowA['ID'],
-                "text"=>$rowA['TEXT'],
-                "status"=>$rowA['STATUS']
+    if ($num > 0) {
+        while ($row = $stmtQ->fetch(PDO::FETCH_ASSOC)) {
+
+            $stmtA = $questionObj->getAnswers($row['ID']);
+
+            $answers = array();
+            while ($rowA = $stmtA->fetch(PDO::FETCH_ASSOC)) {
+                $answer = array(
+                    "id" => $rowA['ID'],
+                    "text" => $rowA['TEXT'],
+                    "status" => $rowA['STATUS']
+                );
+
+                array_push($answers, $answer);
+            }
+
+            $stmtC = $category->getCategory($row['ID_CATEGORY']);
+
+            $question = array(
+                "id" => $row['ID'],
+                "category" => $stmtC->fetch()['NAME'],
+                "text" => $row['TEXT'],
+                "code" => html_entity_decode($row['CODE']),
+                "image" => $row['IMAGE'],
+                "answers" => $answers
             );
-
-            array_push($answers, $answer);
         }
 
-        $stmtC = $category->getCategory($row['ID_CATEGORY']);
+        // set response code - 200 OK
+        http_response_code(200);
 
-        $question=array(
-            "id" => $row['ID'],
-            "category" => $stmtC->fetch()['NAME'],
-            "text" => $row['TEXT'],
-            "code" => html_entity_decode($row['CODE']),
-            "image" => $row['IMAGE'],
-            "answers" => $answers
+        // show products data in json format
+        echo json_encode($question);
+    } else {
+
+        http_response_code(404);
+
+        echo json_encode(
+            array("message" => "No questions found.")
         );
     }
-
-    // set response code - 200 OK
-    http_response_code(200);
-
-    // show products data in json format
-    echo json_encode($question);
 }
 
-else{
-
-    http_response_code(404);
+else {
+    http_response_code(201);
 
     echo json_encode(
-        array("message" => "No questions found.")
+        array("message" => "UnAuthorized")
     );
 }
