773ac95ef4b967ff88ba4a396dd94a36fa77ee71
mk1808 <monikor97@gmail.com>
2019-02-02 18:34:28 +0100

    #61 stworzenie widoku posredniczacego

--- View/src/app/quiz/test/content/test/choose-demo/choose-demo.component.ts
+++ View/src/app/quiz/test/content/test/choose-demo/choose-demo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-choose-demo',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChooseDemoComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router, private route: ActivatedRoute,
+    private cookie: CookieService) { }
+  tests: string[] = ["Test z technologii sieci WEB", "Test z języka Java"]
   ngOnInit() {
+    if (this.cookie.get('user') == "") {
+
+    }
   }
 
+  onTest(id: string) {
+    
+    if (id=="1"){
+    this.router.navigate(['/quiz/demo/java/begin']);
+    
+  }
+  else {
+    this.router.navigate(['/quiz/demo/web/begin']);
+  }
+}
 }
