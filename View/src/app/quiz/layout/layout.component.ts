0557f37b2d23df13864d77dc2eab6e7d0926b455
marqos12 <marqos111222@gmail.com>
2019-02-05 17:49:52 +0100

    #39 poprawa wyświetlania wysokości strony

--- View/src/app/quiz/layout/layout.component.ts
+++ View/src/app/quiz/layout/layout.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2, AfterViewChecked, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
-
+export class LayoutComponent implements OnInit,AfterContentChecked {
+  height=0;
+  @ViewChild('header') header:ElementRef;
+  @ViewChild('footer') footer:ElementRef;
+  @ViewChild('content') content;
   constructor() { }
 
   ngOnInit() {
     
+  }
+
+  ngAfterContentChecked(){
+    this.height=(window.innerHeight-this.header.nativeElement.scrollHeight-this.footer.nativeElement.scrollHeight);
   }
 
 }
