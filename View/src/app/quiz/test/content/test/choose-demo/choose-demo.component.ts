945965cced25d6556355f116f1d2211ec0300edf
mk1808 <monikor97@gmail.com>
2019-02-06 17:40:14 +0100

    #61 zmiana wyglądu wyboru testu demo

--- View/src/app/quiz/test/content/test/choose-demo/choose-demo.component.ts
+++ View/src/app/quiz/test/content/test/choose-demo/choose-demo.component.ts
@@ -11,21 +11,17 @@ export class ChooseDemoComponent implements OnInit {
 
   constructor(private router: Router, private route: ActivatedRoute,
     private cookie: CookieService) { }
-  tests: string[] = ["Test z technologii sieci WEB", "Test z języka Java"]
+
   ngOnInit() {
     if (this.cookie.get('user') == "") {
 
     }
   }
 
-  onTest(id: string) {
+  onTestJava() 
+  {  this.router.navigate(['/quiz/demo/java/begin']);}
+  onTestWeb()
+  { this.router.navigate(['/quiz/demo/web/begin']);}
     
-    if (id=="1"){
-    this.router.navigate(['/quiz/demo/java/begin']);
-    
-  }
-  else {
-    this.router.navigate(['/quiz/demo/web/begin']);
-  }
-}
+ 
 }
